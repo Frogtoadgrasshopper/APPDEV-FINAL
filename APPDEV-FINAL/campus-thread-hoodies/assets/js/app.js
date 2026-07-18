58ac9109cb2c7826ccda5aa7a8ddc337710a516f
Cholo Torres <cptorres26@gmail.com>
2026-07-18 21:43:29 +0800

    Delete APPDEV-FINAL directory

--- APPDEV-FINAL/campus-thread-hoodies/assets/js/app.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.querySelector('[data-nav-toggle]');
-const nav = document.querySelector('[data-main-nav]');
-
-if (toggle && nav) {
-    toggle.addEventListener('click', () => {
-        nav.classList.toggle('open');
-    });
-}
